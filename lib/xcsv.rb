6ecd785013c6844386787086ba32de1607aa7ebe
Victor Moroz <v66moroz@gmail.com>
2018-11-18 18:51:07 -0500

    Refactoring

--- lib/xcsv.rb
+++ lib/xcsv.rb
@@ -5,6 +5,16 @@ require 'xcsv/version'
 class XCSV
   include Enumerable
 
+  def self.open(path)
+    xcsv = new(path)
+    xcsv.open
+    begin
+      yield xcsv
+    ensure
+      xcsv.close
+    end
+  end
+
   def each
     while (rec = self.next) do
       yield rec
--- src/lib.rs
+++ src/lib.rs
@@ -12,14 +12,10 @@ use std::ops::{Deref, DerefMut};
 use flate2::read::GzDecoder;
 use regex::Regex;
 
-use helix::{FromRuby, CheckResult};
-use helix::sys::{VALUE};
-
 type CSVIterType = Iterator<Item=Result<csv::StringRecord, csv::Error>>;
 
 struct CSVIter {
     iter: Box<CSVIterType>,
-    path: String,
 }
 
 impl Deref for CSVIter {
@@ -38,7 +34,7 @@ impl DerefMut for CSVIter {
 
 impl std::fmt::Debug for CSVIter {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "CSVIter: {}", self.path)
+        write!(f, "CSVIter")
     }
 }
 
@@ -48,61 +44,62 @@ impl Clone for CSVIter {
     }
 }
 
-impl FromRuby for CSVIter {
-    type Checked = CSVIter;
-
-    fn from_ruby(value: VALUE) -> CheckResult<CSVIter> {
-        let checked_path = String::from_ruby(value)?;
-
-        let path = String::from_checked(checked_path);
-
-        let gz_regex = Regex::new("\\.gz\\z").unwrap();
-
-        let buf_reader = 
-            match File::open(path.clone()) {
-                Ok(f)   => BufReader::new(f),
-                Err(e)  => raise!(format!("Error while opening file: {}", e)),
-            };
-
-        let gz_reader: Box<Read> =
-            if gz_regex.is_match(&path) {
-                Box::new(GzDecoder::new(buf_reader)) 
-            } else {
-                Box::new(buf_reader)  
-            };
-
-        let csv_reader =
-            csv::ReaderBuilder::new()
-                .has_headers(false)
-                .from_reader(gz_reader);
-
-        Ok(CSVIter{iter: Box::new(csv_reader.into_records()), path: path})
-    }
-
-    fn from_checked(checked: CSVIter) -> CSVIter {
-        checked
-    }
-}
-
 ruby! {
     class XCSV {
         struct {
-            iter: CSVIter,
+            path: String,
+            iter: Option<CSVIter>,
         }
 
-        def initialize(helix, iter: CSVIter) {
-            XCSV { helix, iter }
+        def initialize(helix, path: String) {
+            XCSV { helix, path, iter: None }
+        }
+
+        def open(&mut self) -> Result<(), helix::Error> {
+            self.iter = None;
+
+            let gz_regex = Regex::new("\\.gz\\z").unwrap();
+
+            let buf_reader = 
+                match File::open(&self.path) {
+                    Ok(f)   => BufReader::new(f),
+                    Err(e)  => raise!(e.to_string()),
+                };
+
+            let gz_reader: Box<Read> =
+                if gz_regex.is_match(&self.path) {
+                    Box::new(GzDecoder::new(buf_reader)) 
+                } else {
+                    Box::new(buf_reader)  
+                };
+
+            let csv_reader =
+                csv::ReaderBuilder::new()
+                    .has_headers(false)
+                    .from_reader(gz_reader);
+
+            self.iter = Some(CSVIter{iter: Box::new(csv_reader.into_records())});
+            Ok(())
         }
 
         def next(&mut self) -> Result<Option<Vec<String>>, helix::Error> {
-            match self.iter.next() {
-                Some(Ok(record)) =>
-                    Ok(Some(record.iter().map(|s| s.to_string()).collect())), 
-                Some(Err(e)) =>
-                    raise!(e.to_string()),
+            match self.iter {
+                Some(ref mut iter) =>
+                    match iter.next() {
+                        Some(Ok(record)) =>
+                            Ok(Some(record.iter().map(|s| s.to_string()).collect())), 
+                        Some(Err(e)) =>
+                            raise!(e.to_string()),
+                        None =>
+                            Ok(None)
+                    }
                 None =>
-                    Ok(None)
+                    raise!("closed file")
             }
+        }
+
+        def close(&mut self) -> () {
+            self.iter = None
         }
     }
 }
